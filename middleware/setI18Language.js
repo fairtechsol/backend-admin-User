f7c24d481cb08990fba601bd6976d9effd053b1e
rohanduple <150104513+rohanduple@users.noreply.github.com>
2023-11-24 13:01:48 +0530

    remove catchasync file

--- middleware/setI18Language.js
+++ middleware/setI18Language.js
@@ -1,16 +1,19 @@
 const i18n = require("../config/i18n");
-const catchAsyncErrors = require("../utils/catchAsyncErrors");
 
-const setI18Language = catchAsyncErrors(async (req, res, next) => {
-  const userLang = req.headers["accept-language"];
+const setI18Language = async (req, res, next) => {
+  try {
+    const userLang = req.headers["accept-language"];
 
-  // Extract the primary language from the 'accept-language' header
-  const lang = userLang ? userLang.split(",")[0] : "en";
+    // Extract the primary language from the 'accept-language' header
+    const lang = userLang ? userLang.split(",")[0] : "en";
 
-  // Set the locale for this request
-  i18n.setLocale(req, lang);
+    // Set the locale for this request
+    i18n.setLocale(req, lang);
 
-  next();
-});
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = setI18Language;
