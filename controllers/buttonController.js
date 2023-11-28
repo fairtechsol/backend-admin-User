ec8e0e6ea693c55f7f80ae945968392324edea29
dipak-DEV-3055 <dipak.duple@gmail.com>
2023-11-28 13:47:42 +0530

    Add new button insertion API endpoint

--- controllers/buttonController.js
+++ controllers/buttonController.js
@@ -1,4 +1,4 @@
-const { defaultButtonValue, buttonType } = require('../config/contants');
+const { defaultButtonValue, buttonType, userRoleConstant } = require('../config/contants');
 const buttonService = require('../services/buttonService');
 const {ErrorResponse,SuccessResponse} = require('../utils/response')
 
@@ -11,16 +11,24 @@ exports.getAllButtons = async (req, res) => {
 };
 
 exports.insertButtons = async (req, res) => {
-
-    const buttons = [
+try {
+    let {id,type,value} = req.body
+    if(req.user.roleName != userRoleConstant.user) {
+        return ErrorResponse({ statusCode: 400, message: { msg: "button.InvalidUser" } }, req, res)
+    }
+    const buttonData =
         {
-            type : buttonType.MATCH,
-            value : defaultButtonValue.buttons,
-            createBy : '6f6f9466-25ca-4fa9-af12-271beedc8a17'
-            }
-    ]
-    const button = await buttonService.insertButton(buttons);
+            id,
+            type,
+            value,
+            createBy : req.user.id
+        }
+    const button = await buttonService.addButton(buttonData);
     return SuccessResponse({statusCode : 200,message :{msg:"login"},data : button},req,res);   
+} catch (error) {
+    return ErrorResponse(error, req, res)
+}
+    
   
 };
 
