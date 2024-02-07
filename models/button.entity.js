c34ad3594f880064ff108e43a0a98fc963b5f669
DupleA <amit.dupleit@gmail.com>
2024-02-07 18:13:00 +0530

    fix duplicate err

--- models/button.entity.js
+++ models/button.entity.js
@@ -25,13 +25,7 @@ const buttonSchema = new EntitySchema({
             },
         },
     },
-    indices: [
-        {
-            name: 'button_createBy',   // index name should be start with the table name
-            unique: false, // Optional: Set to true if you want a unique index
-            columns: ['createBy'],
-        }
-    ],
+ 
 });
 
 module.exports = buttonSchema;
