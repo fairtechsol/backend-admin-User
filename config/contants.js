e9eede67e3d700a97a270e63485483d7e19d9d74
nain4884 <nain4884@gmail.com>
2024-04-05 10:02:20 +0530

    redis ttl 24 to 12

--- config/contants.js
+++ config/contants.js
@@ -36,7 +36,7 @@ module.exports.fileType = {
   excel: "excel",
 };
 
-module.exports.redisTimeOut = 24 * 60 * 60;
+module.exports.redisTimeOut = 12 * 60 * 60;
 
 module.exports.matchComissionTypeConstant = {
   totalLoss: "totalLoss",
