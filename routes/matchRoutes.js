e396afaa7a6822057beec33ad8ea46bc5c04e805
GauravDuple <gauravmishra.duple@gmail.com>
2024-05-20 20:23:53 +0530

    create match api changes

--- routes/matchRoutes.js
+++ routes/matchRoutes.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const validator = require('../middleware/joi.validator')
 const { isAuthenticate } = require('../middleware/auth');
 const { matchDetails, listMatch,matchDetailsForFootball } = require('../controllers/matchController');
-const { addMatch } = require('../controllers/expertController');
-const { addMatchValidate } = require('../validators/matchValidator');
+const { addMatch, raceAdd } = require('../controllers/expertController');
+const { addMatchValidate, addRaceValidate } = require('../validators/matchValidator');
 
 router.get('/list', isAuthenticate, listMatch);
 router.get('/:id', isAuthenticate, matchDetails);
 router.get('/other/:id', isAuthenticate, matchDetailsForFootball);
 router.post('/add', validator(addMatchValidate), addMatch);
+router.post('/raceAdd', validator(addRaceValidate), raceAdd);
+
 
 module.exports = router;
--- validators/matchValidator.js
+++ validators/matchValidator.js
@@ -50,4 +50,62 @@ module.exports.addMatchValidate = Joi.object({
   }).messages({
     "object.base": "Invalid input. Please provide a valid object.",
   });
+
+
+
+  module.exports.addRaceValidate = Joi.object({
+    id: Joi.string().guid({ version: 'uuidv4' }).required().messages({
+        "any.required": "Match id is required",
+      }),
+    matchType: Joi.string().required().messages({
+      "string.base": "Match type must be a string",
+      "any.required": "Match type is required",
+    }),
+    createBy: Joi.string().required().messages({
+      "date.base": "Start date must be a valid date",
+      "any.required": "Start date is required",
+    }),
+    competitionId: Joi.string().required().messages({
+      "string.base": "Competition ID must be a string",
+      "any.required": "Competition ID is required",
+    }),
+    competitionName: Joi.string().required().messages({
+      "string.base": "Competition name must be a string",
+      "any.required": "Competition name is required",
+    }),
+    title: Joi.string().required().messages({
+      "string.base": "Title must be a string",
+      "any.required": "Title is required",
+    }),
+    marketId: Joi.string().required().messages({
+      "string.base": "Market ID must be a string",
+      "any.required": "Market ID is required",
+    }),
+    eventId: Joi.string().required().messages({
+      "string.base": "Event ID must be a string",
+      "any.required": "Event ID is required",
+    }),
+    startAt: Joi.date().required().messages({
+      "date.base": "Start date must be a valid date",
+      "any.required": "Start date is required",
+    }),
+    venue: Joi.string().required().messages({
+      "string.base": "venue must be a string",
+      "any.required": "venue is required",
+    }),
+    raceType: Joi.string().required().messages({
+      "string.base": "raceType must be a string",
+      "any.required": "raceType is required",
+    }),
+    countryCode: Joi.string().required().messages({
+      "string.base": "countryCode must be a string",
+      "any.required": "countryCode is required",
+    }),
+    createdAt: Joi.date().required().messages({
+        "date.base": "Created at date must be a valid date",
+        "any.required": "Created at date is required",
+      })
+  }).messages({
+    "object.base": "Invalid input. Please provide a valid object.",
+  });
   
