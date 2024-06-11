8df4b194d866e8eed51b3f5ff083fec5a2a81565
rohanduple <150104513+rohanduple@users.noreply.github.com>
2024-06-11 13:16:31 +0530

    Bug fixed in bet placed of andar bahar

--- controllers/betPlacedController.js
+++ controllers/betPlacedController.js
@@ -3273,7 +3273,7 @@ const validateCardBettingDetails = async (match, betObj, selectionId) => {
     };
   }
 
-  if (betObj?.odds != currData?.rate || (match?.type == cardGameType.abj && betObj?.odds != currData?.b1)) {
+  if ((betObj?.odds != currData?.rate && match?.type != cardGameType.abj) || (match?.type == cardGameType.abj && parseFloat(betObj?.odds) != parseFloat(currData?.b1))) {
     throw {
       statusCode: 400,
       message: {
