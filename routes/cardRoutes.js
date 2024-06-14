3ccb237a45a97ac9ccac6c96630894ee51c9dc39
rohanduple <150104513+rohanduple@users.noreply.github.com>
2024-06-14 13:09:32 +0530

    Added get single bet result api

--- routes/cardRoutes.js
+++ routes/cardRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { isAuthenticate } = require("../middleware/auth");
-const { getCardResultByFGWallet } = require("../controllers/cardController");
+const { getCardResultByFGWallet, getCardResultDetailByFGWallet } = require("../controllers/cardController");
 const router = express.Router();
 
 router.get("/result/:type", isAuthenticate, getCardResultByFGWallet);
+router.get("/result/detail/:id", isAuthenticate, getCardResultDetailByFGWallet);
 
 module.exports = router;
