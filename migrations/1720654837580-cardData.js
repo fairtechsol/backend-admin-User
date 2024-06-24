b01b0a6bf0816e73989fccd4f5b5dd39cf5176bc
rohanduple <150104513+rohanduple@users.noreply.github.com>
2024-06-24 12:34:52 +0530

    Added card matches

--- migrations/1720654837580-cardData.js
+++ migrations/1720654837580-cardData.js
@@ -1,13 +1,15 @@
 const { MigrationInterface, QueryRunner } = require("typeorm");
 const { cardGames } = require("../config/contants");
 
-module.exports = class CardData1717654837580 {
+module.exports = class CardData1720654837580 {
+    name = 'InsertCard1720654099131'
 
     async up(queryRunner) {
         for (let game of cardGames) {
             await queryRunner.query(`INSERT INTO "cardMatchs" (name, type, "minBet", "maxBet", id) SELECT $1, $2, $3, $4, $5 FROM (VALUES (1)) AS dummy WHERE NOT EXISTS (SELECT * FROM "cardMatchs" WHERE type = $2);`,
                 [game.name, game.type, 0, 1, game.id]);
         }
+
     }
 
     async down(queryRunner) {
