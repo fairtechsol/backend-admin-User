5d6d4be605b7a3792351e5c014f4732b6070fc39
rohanduple <150104513+rohanduple@users.noreply.github.com>
2024-10-11 11:48:12 +0530

    Bug fixed in result declare bookmaker 2

--- services/betPlacedService.js
+++ services/betPlacedService.js
@@ -174,7 +174,7 @@ exports.getMultipleAccountProfitLoss = async (betId, userId) => {
 exports.getMultipleAccountMatchProfitLoss = async (betId, userId) => {
 
   const matchTypes = [
-    [matchBettingType.bookmaker, matchBettingType.quickbookmaker1, matchBettingType.quickbookmaker2, matchBettingType.quickbookmaker3, matchBettingType.matchOdd],
+    [matchBettingType.bookmaker, matchBettingType.bookmaker2, matchBettingType.quickbookmaker1, matchBettingType.quickbookmaker2, matchBettingType.quickbookmaker3, matchBettingType.matchOdd],
     [matchBettingType.tiedMatch1, matchBettingType.tiedMatch2, matchBettingType.tiedMatch3],
     [matchBettingType.completeMatch, matchBettingType.completeManual, matchBettingType.completeMatch1],
   ];
