c4e208dc455a2f5c8af8d65fb9e3f3debf37b7af
rohanduple <150104513+rohanduple@users.noreply.github.com>
2024-11-20 15:32:30 +0530

    Bug fixed in verify mfa middleware

--- middleware/verifyMac88MFA.js
+++ middleware/verifyMac88MFA.js
@@ -5,14 +5,14 @@ const crypto = require("crypto");
 // Middleware function for rate limiting
 const verifyRSA = async (req, res, next) => {
     try {
-        const { Signature } = req.headers;
+        const { signature } = req.headers;
         const data = req.body;
         const publicKey = fs.readFileSync("mac88casinoPublic.pem", "utf8");
         const verifier = crypto.createVerify('sha256'); // Use the same hash algorithm
         verifier.update(data);
         verifier.end();
 
-        const isValid = verifier.verify(publicKey, Signature, 'base64');
+        const isValid = verifier.verify(publicKey, signature, 'base64');
         if (!isValid) {
             return res.status(400).json({ status: "OP_INVALID_SIGNATURE" })
         }
