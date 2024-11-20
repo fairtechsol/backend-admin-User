c7f8aa3176a8b1c4a5c8b46b2fcf97746b674990
rohanduple <150104513+rohanduple@users.noreply.github.com>
2024-11-20 10:59:20 +0530

    Make new function for result declare

--- controllers/mac88CasinoController.js
+++ controllers/mac88CasinoController.js
@@ -1,8 +1,8 @@
-const { ILike } = require("typeorm");
+const { ILike, Between } = require("typeorm");
 const { mac88Domain, mac88CasinoOperatorId, socketData, transType, userRoleConstant, walletDomain } = require("../config/contants");
 const { getUserRedisData, incrementValuesRedis } = require("../services/redis/commonfunction");
 const { getTransactions, getTransaction, updateTransactionData, addTransaction } = require("../services/transactionService");
-const { updateUserBalanceData } = require("../services/userBalanceService");
+const { updateUserBalanceData, getUserBalanceDataByUserId } = require("../services/userBalanceService");
 const { getUserById, getUserWithUserBalance, getUserDataWithUserBalance, getParentsWithBalance } = require("../services/userService");
 const { addVirtualCasinoBetPlaced, getVirtualCasinoBetPlaced, updateVirtualCasinoBetPlaced } = require("../services/virtualCasinoBetPlacedsService");
 const { sendMessageToUser } = require("../sockets/socketManager");
@@ -20,6 +20,7 @@ exports.loginMac88Casino = async (req, res) => {
             return ErrorResponse({ statusCode: 403, message: { msg: "user.demoNoAccess" } }, req, res);
         }
         const userRedisData = await getUserRedisData(user.id);
+        const userCurrBalance = parseInt(userRedisData?.currentBalance || 0) - parseInt(userRedisData?.exposure || 0);
 
         let casinoData = {
             "operatorId": mac88CasinoOperatorId,
@@ -31,12 +32,17 @@ exports.loginMac88Casino = async (req, res) => {
             "lobby": false,
             "clientIp": "52.56.207.91",
             "currency": "INR",
-            "balance": parseInt(userRedisData?.currentBalance || 0) - parseInt(userRedisData?.exposure || 0),
+            "balance": userCurrBalance,
             "redirectUrl": "https://devmaxbet9api.fairgame.club"
         }
         let result;
         if (userRedisData) {
             result = await apiCall(apiMethod.post, mac88Domain + allApiRoutes.MAC88.login, casinoData, { Signature: generateRSASignature(JSON.stringify(casinoData)) });
+        }
+
+        const userTransaction = await getTransaction({ type: 3, searchId: user.id, createdAt: Between(new Date(new Date().setHours(0, 0, 0, 0)), new Date(new Date().setHours(23, 59, 59, 99))) });
+        if (!userTransaction) {
+            await addTransaction({ searchId: user.id, type: 3, userId: user.id, actionBy: user.id, amount: 0, closingBalance: userCurrBalance, transType: transType.win, description: `${new Date()}` });
         }
 
         return SuccessResponse(
@@ -155,139 +161,18 @@ exports.getBetsMac88 = async (req, res) => {
 exports.resultRequestMac88 = async (req, res) => {
     try {
         const { userId, creditAmount, gameId, roundId, transactionId } = req.body;
-        let superAdminData = {};
-        const user = await getUserDataWithUserBalance({ id: userId });
-        if (!user) {
-            return res.status(400).json({
-                "status": "OP_USER_NOT_FOUND"
-            });
-        }
         const userRedisData = await getUserRedisData(userId);
-
-        const userPrevBetPlaced = await getVirtualCasinoBetPlaced({ transactionId: transactionId });
-        const userCurrProfitLoss = parseFloat(creditAmount) - parseFloat(userPrevBetPlaced.amount);
-        const userCurrBalance = parseFloat(user?.userBal?.currentBalance) + parseFloat(creditAmount)
-        //getting wallet profitloss
-        const fwProfitLoss = parseFloat(((-userCurrProfitLoss * user.fwPartnership) / 100).toString());
-
-        logger.info({
-            message: `User balance and profit loss during declare of virtual casino for user ${userId}: `,
-            data: {
-                profitloss: userCurrProfitLoss,
-                userBalance: userCurrBalance,
-                fwProfitLoss: fwProfitLoss
-            }
-        });
-
-        await updateUserBalanceData(user.id, {
-            profitLoss: userCurrProfitLoss,
-            myProfitLoss: userCurrProfitLoss,
-            balance: parseFloat(creditAmount)
-        });
-
-        if (userRedisData) {
-
-            await incrementValuesRedis(user.id, {
-                profitLoss: userCurrProfitLoss,
-                myProfitLoss: userCurrProfitLoss,
-                currentBalance: parseFloat(creditAmount)
-            });
+        let currUserData;
+        if (!userRedisData) {
+            currUserData = await getUserBalanceDataByUserId(userId, ["currentBalance","exposure"])
         }
 
-        sendMessageToUser(
-            userId,
-            socketData.userBalanceUpdateEvent,
-            { currentBalance: userCurrBalance }
-        );
+        const balance = parseFloat(userRedisData?.currentBalance ?? currUserData?.currentBalance) - parseFloat(userRedisData?.exposure ?? currUserData?.exposure) + parseFloat(creditAmount);
 
-        updateVirtualCasinoBetPlaced({ transactionId: transactionId }, { amount: userCurrProfitLoss });
-        const currGameTransaction = await getTransaction({ searchId: user.id, description: ILike(`%${gameId}-${roundId}`) });
-        if (currGameTransaction) {
-            await updateTransactionData(currGameTransaction?.id, userCurrProfitLoss);
-        }
-        else {
-            await addTransaction({ searchId: user.id, type: 3, userId: user.id, actionBy: user.id, amount: userCurrProfitLoss, closingBalance: userCurrBalance, transType: userCurrProfitLoss < 0 ? transType.loss : transType.win, description: `${gameId}-${roundId}` });
-        }
-
-        if (user.createBy === user.id) {
-            superAdminData[user.id] = {
-                role: user.roleName,
-                balance: userCurrBalance,
-                profitLoss: userCurrProfitLoss,
-                myProfitLoss: userCurrProfitLoss,
-            };
-        }
-
-        let parentUsers = await getParentsWithBalance(user.id);
-        for (const patentUser of parentUsers) {
-            let upLinePartnership = 100;
-            if (patentUser.roleName === userRoleConstant.superAdmin) {
-                upLinePartnership = user.fwPartnership + user.faPartnership;
-            } else if (patentUser.roleName === userRoleConstant.admin) {
-                upLinePartnership = user.fwPartnership + user.faPartnership + user.saPartnership;
-            } else if (patentUser.roleName === userRoleConstant.superMaster) {
-                upLinePartnership = user.fwPartnership + user.faPartnership + user.saPartnership + user.aPartnership;
-            } else if (patentUser.roleName === userRoleConstant.master) {
-                upLinePartnership = user.fwPartnership + user.faPartnership + user.saPartnership + user.aPartnership + user.smPartnership;
-            }
-            else if (patentUser.roleName === userRoleConstant.agent) {
-                upLinePartnership = user.fwPartnership + user.faPartnership + user.saPartnership + user.aPartnership + user.smPartnership + user.mPartnership;
-            }
-
-            let myProfitLoss = parseFloat(
-                (((userCurrProfitLoss) * upLinePartnership) / 100).toString()
-            );
-
-            await updateUserBalanceData(patentUser.id, {
-                profitLoss: userCurrProfitLoss,
-                myProfitLoss: -myProfitLoss,
-                balance: 0
-            });
-            let parentUserRedisData = await getUserRedisData(patentUser.id);
-            if (parentUserRedisData) {
-
-                await incrementValuesRedis(patentUser.id, {
-                    profitLoss: userCurrProfitLoss,
-                    myProfitLoss: -myProfitLoss,
-                });
-            }
-
-            logger.info({
-                message: `User balance and profit loss during declare of virtual casino for parent ${patentUser.id}: `,
-                data: {
-                    profitloss: userCurrProfitLoss,
-                    myProfitLoss:-myProfitLoss,
-                }
-            });
-            if (patentUser.createBy === patentUser.id) {
-                superAdminData[patentUser.id] = {
-                    balance: 0,
-                    profitLoss: userCurrProfitLoss,
-                    myProfitLoss: -myProfitLoss,
-                    role: patentUser.roleName,
-                };
-            }
-        }
-
-        let walletData = {
-            profitLoss: userCurrProfitLoss,
-            fairgameAdminPL: user.superParentType == userRoleConstant.fairGameAdmin ? {
-                id: user.superParentId,
-                myProfitLoss: -parseFloat(
-                    (((userCurrProfitLoss) * user.faPartnership) / 100).toString()
-                )
-            } : null,
-            fairgameWalletPL: fwProfitLoss,
-            superAdminData: superAdminData
-        }
-        logger.info({
-            message: `wallet data for virtual casino result declare: `,
-            data: walletData
-        })
-        apiCall(apiMethod.post, walletDomain + allApiRoutes.WALLET.virtualCasinoResult, walletData);
+        calculateMac88ResultDeclare(userId, creditAmount, gameId, roundId, transactionId, userRedisData);
 
         return res.status(200).json({
-            "balance": userCurrBalance,
+            "balance": balance,
             "status": "OP_SUCCESS"
         });
     }
@@ -301,6 +186,139 @@ exports.resultRequestMac88 = async (req, res) => {
             res
         );
     }
+}
+
+const calculateMac88ResultDeclare = async (userId, creditAmount, gameId, roundId, transactionId, userRedisData) => {
+    let superAdminData = {};
+    const user = await getUserDataWithUserBalance({ id: userId });
+    if (!user) {
+        return res.status(400).json({
+            "status": "OP_USER_NOT_FOUND"
+        });
+    }
+
+    const userPrevBetPlaced = await getVirtualCasinoBetPlaced({ transactionId: transactionId });
+    const userCurrProfitLoss = parseFloat(creditAmount) - parseFloat(userPrevBetPlaced.amount);
+    const userCurrBalance = parseFloat(user?.userBal?.currentBalance) + parseFloat(creditAmount)
+    //getting wallet profitloss
+    const fwProfitLoss = parseFloat(((-userCurrProfitLoss * user.fwPartnership) / 100).toString());
+
+    logger.info({
+        message: `User balance and profit loss during declare of virtual casino for user ${userId}: `,
+        data: {
+            profitloss: userCurrProfitLoss,
+            userBalance: userCurrBalance,
+            fwProfitLoss: fwProfitLoss
+        }
+    });
+
+    await updateUserBalanceData(user.id, {
+        profitLoss: userCurrProfitLoss,
+        myProfitLoss: userCurrProfitLoss,
+        balance: parseFloat(creditAmount)
+    });
+
+    if (userRedisData) {
+
+        await incrementValuesRedis(user.id, {
+            profitLoss: userCurrProfitLoss,
+            myProfitLoss: userCurrProfitLoss,
+            currentBalance: parseFloat(creditAmount)
+        });
+    }
+
+    sendMessageToUser(
+        userId,
+        socketData.userBalanceUpdateEvent,
+        { currentBalance: userCurrBalance }
+    );
+
+    updateVirtualCasinoBetPlaced({ transactionId: transactionId }, { amount: userCurrProfitLoss });
+    
+    const userTransaction = await getTransaction({ type: 3, searchId: user.id, createdAt: Between(new Date(new Date().setHours(0, 0, 0, 0)), new Date(new Date().setHours(23, 59, 59, 99))) });
+    if (!userTransaction) {
+        await addTransaction({ searchId: user.id, type: 3, userId: user.id, actionBy: user.id, amount: 0, closingBalance: userCurrBalance, transType: transType.win, description: `${new Date()}` });
+    } else {
+        await updateTransactionData(userTransaction?.id, userCurrProfitLoss);
+    }
+
+    if (user.createBy === user.id) {
+        superAdminData[user.id] = {
+            role: user.roleName,
+            balance: userCurrBalance,
+            profitLoss: userCurrProfitLoss,
+            myProfitLoss: userCurrProfitLoss,
+        };
+    }
+
+    let parentUsers = await getParentsWithBalance(user.id);
+    for (const patentUser of parentUsers) {
+        let upLinePartnership = 100;
+        if (patentUser.roleName === userRoleConstant.superAdmin) {
+            upLinePartnership = user.fwPartnership + user.faPartnership;
+        } else if (patentUser.roleName === userRoleConstant.admin) {
+            upLinePartnership = user.fwPartnership + user.faPartnership + user.saPartnership;
+        } else if (patentUser.roleName === userRoleConstant.superMaster) {
+            upLinePartnership = user.fwPartnership + user.faPartnership + user.saPartnership + user.aPartnership;
+        } else if (patentUser.roleName === userRoleConstant.master) {
+            upLinePartnership = user.fwPartnership + user.faPartnership + user.saPartnership + user.aPartnership + user.smPartnership;
+        }
+        else if (patentUser.roleName === userRoleConstant.agent) {
+            upLinePartnership = user.fwPartnership + user.faPartnership + user.saPartnership + user.aPartnership + user.smPartnership + user.mPartnership;
+        }
+
+        let myProfitLoss = parseFloat(
+            (((userCurrProfitLoss) * upLinePartnership) / 100).toString()
+        );
+
+        await updateUserBalanceData(patentUser.id, {
+            profitLoss: userCurrProfitLoss,
+            myProfitLoss: -myProfitLoss,
+            balance: 0
+        });
+
+        let parentUserRedisData = await getUserRedisData(patentUser.id);
+        if (parentUserRedisData) {
+
+            await incrementValuesRedis(patentUser.id, {
+                profitLoss: userCurrProfitLoss,
+                myProfitLoss: -myProfitLoss,
+            });
+        }
+
+        logger.info({
+            message: `User balance and profit loss during declare of virtual casino for parent ${patentUser.id}: `,
+            data: {
+                profitloss: userCurrProfitLoss,
+                myProfitLoss: -myProfitLoss,
+            }
+        });
+        if (patentUser.createBy === patentUser.id) {
+            superAdminData[patentUser.id] = {
+                balance: 0,
+                profitLoss: userCurrProfitLoss,
+                myProfitLoss: -myProfitLoss,
+                role: patentUser.roleName,
+            };
+        }
+    }
+
+    let walletData = {
+        profitLoss: userCurrProfitLoss,
+        fairgameAdminPL: user.superParentType == userRoleConstant.fairGameAdmin ? {
+            id: user.superParentId,
+            myProfitLoss: -parseFloat(
+                (((userCurrProfitLoss) * user.faPartnership) / 100).toString()
+            )
+        } : null,
+        fairgameWalletPL: fwProfitLoss,
+        superAdminData: superAdminData
+    }
+    logger.info({
+        message: `wallet data for virtual casino result declare: `,
+        data: walletData
+    });
+    apiCall(apiMethod.post, walletDomain + allApiRoutes.WALLET.virtualCasinoResult, walletData);
 }
 
 exports.rollBackRequestMac88 = async (req, res) => {
