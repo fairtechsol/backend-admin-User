3efb567acc4f26d5e5eb85a4e4d26ac44cc4d9f5
root <root@ip-172-31-21-135.eu-west-2.compute.internal>
2024-11-20 10:10:37 +0000

    fixed in verify

--- middleware/verifyMac88MFA.js
+++ middleware/verifyMac88MFA.js
@@ -7,18 +7,22 @@ const crypto = require("crypto");
 const verifyRSA = async (req, res, next) => {
     try {
         const { signature } = req.headers;
+
         const data = req.body;
         const publicKey = fs.readFileSync("mac88casinoPublic.pem", "utf8");
-        const verifier = crypto.createVerify('sha256'); // Use the same hash algorithm
-        verifier.update(JSON.stringify(data));
-        verifier.end();
 
+        verifier.update(JSON.stringify(data));
+const verifier = crypto.createVerify('sha256'); // Use the same hash algorithm
+        verifier.end();
+console.log(publicKey,signature,data)
         const isValid = verifier.verify(publicKey, signature, 'base64');
+console.log(isValid)
         if (!isValid) {
             return res.status(400).json({ status: "OP_INVALID_SIGNATURE" })
         }
         next(); // Proceed to the next middleware
     } catch (error) {
+console.log(error)
         return ErrorResponse({ statusCode: 400, message: { msg: "internalServerError" } }, req, res);
     }
 };
