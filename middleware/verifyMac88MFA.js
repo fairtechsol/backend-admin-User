f634da93d2de7bd4f6b90b50ae05ff37dce26d58
rohanduple <150104513+rohanduple@users.noreply.github.com>
2024-11-22 16:05:04 +0530

    Removed console from verify mac 88 middleware

--- middleware/verifyMac88MFA.js
+++ middleware/verifyMac88MFA.js
@@ -11,20 +11,19 @@ const verifyRSA = async (req, res, next) => {
         const data = req.body;
         const publicKey = fs.readFileSync("mac88casinoPublic.pem", "utf8");
 
-	const verifier = crypto.createVerify('sha256'); // Use the same hash algorithm
+	    const verifier = crypto.createVerify('sha256'); // Use the same hash algorithm
         verifier.update(JSON.stringify(data));
 
         verifier.end();
-console.log(publicKey,signature,data)
         const isValid = verifier.verify(publicKey, signature, 'base64');
-console.log(isValid)
         if (!isValid) {
             return res.status(400).json({ status: "OP_INVALID_SIGNATURE" })
         }
         next(); // Proceed to the next middleware
     } catch (error) {
-console.log(error)
-        return ErrorResponse({ statusCode: 400, message: { msg: "internalServerError" } }, req, res);
+        return res.status(500).json({
+            "status": "OP_GENERAL_ERROR"
+        });
     }
 };
 
