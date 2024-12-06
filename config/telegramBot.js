34c3a57a6fd6364c34f8c60bd57a9b7874d00c72
rohanduple <150104513+rohanduple@users.noreply.github.com>
2024-12-06 16:38:01 +0530

    Bug fixed in bot

--- config/telegramBot.js
+++ config/telegramBot.js
@@ -4,7 +4,7 @@ const { connectAppWithToken } = require('../services/commonService');
 const { getRedisKey } = require('../services/redis/commonfunction');
 const { __mf } = require('i18n');
 
-const bot = new TelegramBot(process.env.TELEGRAM_BOT);
+const bot = new TelegramBot("7987516913:AAFZw23Ys56_zmacUdCCU_4uWKuEK4Ehp1k");
 
 bot.onText("/start", (msg) => {
     bot.sendMessage(msg.chat.id, __mf("telegramBot.start"));
@@ -25,4 +25,10 @@ bot.onText(/\/connect/, async (msg) => {
         bot.sendMessage(msg.chat.id, __mf(e?.message?.msg || "internalServerError", e?.message?.keys));
     }
 });
+
+bot.on("polling_error", (msg) => console.log(msg));
+bot.startPolling().catch((error)=>{
+    console.log(error);
+});
+
 module.exports = bot;
