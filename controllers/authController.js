170f98526f2991ca67275321f07201e663fb1fd1
rohanduple <150104513+rohanduple@users.noreply.github.com>
2024-12-09 16:29:34 +0530

    Bug fixed in get auth user

--- controllers/authController.js
+++ controllers/authController.js
@@ -445,7 +445,7 @@ exports.verifyAuthenticatorRefreshToken = async (req, res) => {
     const { id } = req.user;
     const { authToken } = req.body;
 
-    const authDevice = await getAuthenticator({ userId: id }, ["deviceId", "id"]);
+    const authDevice = await getAuthenticator({ userId: id }, ["deviceId", "id","type"]);
     if (!authDevice) {
       return ErrorResponse(
         {
