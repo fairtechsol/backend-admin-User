e63d8aa38ef34a55c8fe592e32e8182ce20a5f37
rohanduple <150104513+rohanduple@users.noreply.github.com>
2024-12-10 13:30:46 +0530

    added encryption

---
+++ middleware/encryptDecryptData.js
@@ -0,0 +1,16 @@
+const { decryptAESKeyWithRSA, decryptWithAES, encryptWithAES, encryptAESKeyWithRSA } = require("../utils/encryptDecrypt");
+
+module.exports = (req, res, next) => {
+    // Decrypt `req.query`
+    if (Object.keys(req.query).length > 0) {
+      const aesKey = decryptAESKeyWithRSA(req.query.encryptedKey);
+      req.query = decryptWithAES(req.query.encryptedData, aesKey);
+    }
+  
+    // Decrypt `req.body`
+    if (Object.keys(req.body).length > 0) {
+      const aesKey = decryptAESKeyWithRSA(req.body.encryptedKey);
+      req.body = decryptWithAES(req.body.encryptedData, aesKey);
+    }
+    next();
+  };
