59468e6a0369bf0a3ea68f84a4afb18bb3342c25
rohanduple <150104513+rohanduple@users.noreply.github.com>
2025-02-06 11:41:18 +0530

    Enhance CheckThirdPartyRate validation to include matchBettingData gtype check for match status

--- controllers/betPlacedController.js
+++ controllers/betPlacedController.js
@@ -1659,7 +1659,7 @@ let CheckThirdPartyRate = async (matchBettingDetail, betObj, teams, isBookmakerM
     );
     let filterData = matchBettingData?.section?.find((item) => item?.sid?.toString() == betObj?.selectionId?.toString());
     if (filterData) {
-      if (!['ACTIVE', 'OPEN', ''].includes(filterData.gstatus) || !['ACTIVE', 'OPEN', ''].includes(matchBettingData.status)) {
+      if (!['ACTIVE', 'OPEN', ''].includes(filterData.gstatus) || (!['ACTIVE', 'OPEN', ''].includes(matchBettingData.status) && matchBettingData?.gtype == "match")) {
         throw {
           statusCode: 400,
           message: {
@@ -1683,8 +1683,8 @@ let CheckThirdPartyRate = async (matchBettingDetail, betObj, teams, isBookmakerM
           };
         }
       }
-      
-     
+
+
       return false;
     }
     return true;
