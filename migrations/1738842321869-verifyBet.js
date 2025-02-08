8a094e7925ca3aab2caa5a4c112cd05ff3c1a4b4
rohanduple <150104513+rohanduple@users.noreply.github.com>
2025-02-08 13:34:12 +0530

    Add verifyBy field to betPlaceds table and update migration for isVerified field

--- migrations/1738842321869-verifyBet.js
+++ migrations/1738842321869-verifyBet.js
@@ -5,9 +5,11 @@ module.exports = class VerifyBet1738842321869 {
 
     async up(queryRunner) {
         await queryRunner.query(`ALTER TABLE "betPlaceds" ADD "isVerified" boolean NOT NULL DEFAULT false`);
+        await queryRunner.query(`ALTER TABLE "betPlaceds" ADD "verifyBy" character varying`);
     }
 
     async down(queryRunner) {
         await queryRunner.query(`ALTER TABLE "betPlaceds" DROP COLUMN "isVerified"`);
+        await queryRunner.query(`ALTER TABLE "betPlaceds" DROP COLUMN "verifyBy"`);
     }
 }
