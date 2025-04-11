0ec72380c27a47d260575ba58ed6077f6044100b
rohanduple <150104513+rohanduple@users.noreply.github.com>
2025-04-11 11:13:43 +0530

    Refactor access user creation validation schema to include transaction password and improve structure

--- validators/accessUserValidator.js
+++ validators/accessUserValidator.js
@@ -1,24 +1,31 @@
-
 const Joi = require('joi');
 const { passwordRegex } = require('../config/contants');
 
 module.exports.CreateAccessUser = Joi.object({
-    userName: Joi.string().trim().required(),
-    fullName: Joi.string().min(3).max(255).allow(""),
-    password: Joi.string().pattern(passwordRegex).required().label('password').messages({
+  userName: Joi.string().trim().required(),
+  fullName: Joi.string().min(3).max(255).allow(""),
+  transactionPassword: Joi.string().required().messages({
+    'any.required': 'Transaction Password is required'
+  }),
+  permission: Joi.object().required().messages({
+    'any.required': 'Permission is required',
+  }),
+  id: Joi.string().optional()
+})
+  .when(Joi.object({ id: Joi.exist() }).unknown(), {
+    then: Joi.object({
+      password: Joi.forbidden(),
+      confirmPassword: Joi.forbidden()
+    }),
+    otherwise: Joi.object({
+      password: Joi.string().pattern(passwordRegex).required().label('password').messages({
         'string.pattern.base': 'user.passwordMatch',
         'any.required': 'Password is required',
-    }),
-    transactionPassword: Joi.string().required().messages({
-        'any.required': 'Transaction Password is required'
-    }),
-    confirmPassword: Joi.string().required().valid(Joi.ref('password')).label('Confirm Password').messages({
+      }),
+      confirmPassword: Joi.string().required().valid(Joi.ref('password')).label('Confirm Password').messages({
         'string.base': 'Confirm Password must be a string',
         'any.required': 'Confirm Password is required',
         'any.only': 'Confirm Password must match Password',
-    }),
-    permission: Joi.object().required().messages({
-        'any.required': 'Permission is required',
-    }),
-    id: Joi.string().optional()
-});
+      })
+    })
+  });
