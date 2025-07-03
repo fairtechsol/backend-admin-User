465af3426881993f83d38895fe001e7d889a7892
Rohan87080 <rohan@fairtechsol.com>
2025-07-03 10:22:22 +0530

    refactor: simplify GrpcClient structure and improve service initialization

--- grpc/grpcClient/grpcClient.js
+++ grpc/grpcClient/grpcClient.js
@@ -3,94 +3,70 @@ const protoLoader = require("@grpc/proto-loader");
 const lodash = require("lodash");
 
 /**
- * GrpcClient class for managing gRPC clients and making RPC calls.
+ * GrpcClient: Singleton-based client manager that avoids recreating channels.
  */
 class GrpcClient {
-  /**
-   * Constructor to initialize the gRPC client.
-   * @param {Array} protoOptionsArray - Array of proto options, each containing the path, package name, and service name.
-   * @param {string} serverAddress - Address of the gRPC server.
-   */
-  constructor(protoOptionsArray, serverAddress) {
-    this.serverAddress = serverAddress;
-    this.clients = this.loadProtoServices(protoOptionsArray);
-  }
+  static serviceConstructors = {};
+  static clientsRegistry = {};
 
-  /**
-   * Load the proto services and create clients for each service.
-   * @param {Array} protoOptionsArray - Array of proto options.
-   * @returns {Object} - Object containing gRPC clients for each service.
-   */
-  loadProtoServices(protoOptionsArray) {
-    const clients = {};
-    protoOptionsArray.forEach((protoOptions) => {
-      const options = {
+  static initialize(protoOptionsArray) {
+    if (Object.keys(GrpcClient.serviceConstructors).length) return;
+    protoOptionsArray.forEach(opts => {
+      const pkgDef = protoLoader.loadSync(opts.path, {
         keepCase: true,
         longs: String,
         enums: String,
         defaults: true,
         oneofs: true,
-        ...(protoOptions.options || {}),
-      };
-      const packageDefinition = protoLoader.loadSync(
-        protoOptions.path,
-        options
-      );
-      const grpcObject = grpc.loadPackageDefinition(packageDefinition);
-      const ServiceCtor = lodash.get(
-        grpcObject,
-        `${protoOptions.package}.${protoOptions.service}`
-      );
-
-      // set unlimited message size on both send and receive
-      const channelOptions = {
-        "grpc.max_receive_message_length": -1, // unlimited inbound
-        "grpc.max_send_message_length":    -1  // unlimited outbound
-      };
-
-      const creds =
-        process.env.NODE_ENV === "production" || process.env.NODE_ENV === "dev"
-          ? grpc.credentials.createSsl()
-          : grpc.credentials.createInsecure();
-
-      clients[protoOptions.service] = new ServiceCtor(
-        this.serverAddress,
-        creds,
-        channelOptions
-      );
+        ...(opts.options || {}),
+      });
+      const grpcObj = grpc.loadPackageDefinition(pkgDef);
+      const ctor = lodash.get(grpcObj, `${opts.package}.${opts.service}`);
+      if (!ctor) throw new Error(`Cannot find ${opts.service} in ${opts.package}`);
+      GrpcClient.serviceConstructors[opts.service] = ctor;
     });
-    return clients;
   }
 
-  /**
-   * Call a method on a gRPC service.
-   * @param {string} serviceName - Name of the gRPC service.
-   * @param {string} methodName - Name of the method to call.
-   * @param {Object} requestData - Data to be sent in the request.
-   * @param {Object} metadata - Metadata to be added to the request.
-   * @returns {Promise} - Promise resolving with the response or rejecting with an error.
-   */
-  async callMethod(serviceName, methodName, requestData, metadata = {}) {
-    return new Promise((resolve, reject) => {
-      const client = this.clients[serviceName];
-      if (!client) {
-        return reject(new Error(`Service ${serviceName} not found`));
-      }
+  constructor(protoOptionsArray, serverAddress) {
+    GrpcClient.initialize(protoOptionsArray);
+    this.serverAddress = serverAddress;
+  }
 
-      // Create and populate metadata
-      const meta = new grpc.Metadata();
-      Object.keys(metadata).forEach((key) => {
-        meta.add(key, metadata[key]);
-      });
-
-      // Call the gRPC method with request data and metadata
-      client[methodName](requestData, meta, (error, response) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(response);
+  getClient(serviceName) {
+    const key = `${serviceName}@${this.serverAddress}`;
+    if (!GrpcClient.clientsRegistry[key]) {
+      const Ctor = GrpcClient.serviceConstructors[serviceName];
+      if (!Ctor) throw new Error(`Service constructor for ${serviceName} not loaded`);
+      const creds = ["production", "dev"].includes(process.env.NODE_ENV)
+        ? grpc.credentials.createSsl()
+        : grpc.credentials.createInsecure();
+      GrpcClient.clientsRegistry[key] = new Ctor(
+        this.serverAddress,
+        creds,
+        {
+          "grpc.max_receive_message_length": -1,
+          "grpc.max_send_message_length": -1,
         }
+      );
+    }
+    return GrpcClient.clientsRegistry[key];
+  }
+
+  callMethod(serviceName, methodName, requestData, metadata = {}) {
+    return new Promise((resolve, reject) => {
+      const client = this.getClient(serviceName);
+      const meta = new grpc.Metadata();
+      Object.entries(metadata).forEach(([k, v]) => meta.add(k, v));
+      client[methodName](requestData, meta, (err, resp) => {
+        if (err) return reject(err);
+        resolve(resp);
       });
+    });
+  }
+
+  static shutdownAll() {
+    Object.values(GrpcClient.clientsRegistry).forEach(c => {
+      try { c.close(); } catch (_) {}
     });
   }
 }
